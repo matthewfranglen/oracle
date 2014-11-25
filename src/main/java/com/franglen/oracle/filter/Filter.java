4c6e2481594f8f830d2ad602634a4a6e3393a6ca
Matthew Franglen <matthew@franglen.org>
2014-11-25 21:49:29 +0000

    Comment on Filter

--- src/main/java/com/franglen/oracle/filter/Filter.java
+++ src/main/java/com/franglen/oracle/filter/Filter.java
@@ -2,12 +2,17 @@ package com.franglen.oracle.filter;
 
 import java.util.Random;
 
-
 /**
+ * A filter represents a constraint on a seed.
+ * 
  * @author matthew
  *
  */
 public interface Filter {
 
+	/**
+	 * The filter accepts the random object if it generates the correct value on
+	 * the next call.
+	 */
 	public boolean accepts(Random value);
 }
