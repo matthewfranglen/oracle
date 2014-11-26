29a10f423564f506f20bb63a2f6d59072aa65888
Matthew Franglen <matthew@franglen.org>
2014-11-26 20:56:27 +0000

    Stop use of boolean enum pattern in SpreadGenerator
    An actual enum that wraps the behaviour is better. The code also
    explains itself better like this.
    
    It would be nice to have a better way to handle the selection of the
    AtomicLong.

--- src/main/java/com/franglen/oracle/generator/SpreadGenerator.java
+++ src/main/java/com/franglen/oracle/generator/SpreadGenerator.java
@@ -1,50 +1,97 @@
 package com.franglen.oracle.generator;
 
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.LongStream;
 
-
 /**
- * Generates values by flipping between the range before and the range after the start.
+ * Generates values by flipping between the range before and the range after the
+ * start.
  * 
  * @author matthew
  */
 public class SpreadGenerator implements Generator {
 
+	private static enum DIRECTION {
+		UP {
+
+			@Override
+			DIRECTION inverse() {
+				return DOWN;
+			}
+
+			@Override
+			AtomicLong getStartHolder(SpreadGenerator generator) {
+				return generator.above;
+			}
+
+			@Override
+			long getAndMoveStart(AtomicLong startHolder, long values) {
+				return startHolder.getAndAdd(values);
+			}
+
+			@Override
+			LongStream stream(long start, long values) {
+				return LongStream.range(start, start + values);
+			}
+		},
+		DOWN {
+
+			@Override
+			DIRECTION inverse() {
+				return UP;
+			}
+
+			@Override
+			AtomicLong getStartHolder(SpreadGenerator generator) {
+				return generator.below;
+			}
+
+			@Override
+			long getAndMoveStart(AtomicLong startHolder, long values) {
+				return startHolder.getAndAdd(-values);
+			}
+
+			@Override
+			LongStream stream(long start, long values) {
+				return LongStream.range(start - values, start);
+			}
+		};
+
+		abstract DIRECTION inverse();
+
+		abstract AtomicLong getStartHolder(SpreadGenerator generator);
+
+		abstract long getAndMoveStart(AtomicLong startHolder, long values);
+
+		abstract LongStream stream(long start, long values);
+	}
+
 	private final AtomicLong above, below;
-	private final AtomicBoolean isAboveNext;
+	private final AtomicReference<DIRECTION> direction;
 
 	public SpreadGenerator(long start) {
 		above = new AtomicLong(start);
 		below = new AtomicLong(start);
-		isAboveNext = new AtomicBoolean(true);
+		direction = new AtomicReference<DIRECTION>(DIRECTION.UP);
 	}
 
 	@Override
 	public LongStream stream(long values) {
-		return getAndToggleIsAboveNext() ? streamAbove(values) : streamBelow(values);
+		DIRECTION direction = getAndToggleDirection();
+		AtomicLong startHolder = direction.getStartHolder(this);
+		long start = direction.getAndMoveStart(startHolder, values);
+		return direction.stream(start, values);
 	}
 
-	private boolean getAndToggleIsAboveNext() {
-		boolean result;
+	private DIRECTION getAndToggleDirection() {
+		DIRECTION result;
 
 		do {
-			result = isAboveNext.get();
-		} while (!isAboveNext.compareAndSet(result, !result));
+			result = direction.get();
+		}
+		while (!direction.compareAndSet(result, result.inverse()));
 
 		return result;
-	}
-
-	private LongStream streamAbove(long values) {
-		long start = this.above.getAndAdd(values);
-
-		return LongStream.range(start, start + values);
-	}
-
-	private LongStream streamBelow(long values) {
-		long start = this.below.getAndAdd(-values);
-
-		return LongStream.range(start - values, start);
 	}
 }
